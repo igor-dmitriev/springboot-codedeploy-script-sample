3c19f6263dab0f195008a1d5b14c872335054b29
Ihor Dmitriiev <i.dmitriiev@spd-ukraine.com>
2020-03-25 00:08:32 +0200

    fix port

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -2,7 +2,7 @@
 
 while [ true ]
 do
-    if [ "$(curl -s http://localhost:80/health)" = '{"status":"UP"}' ]
+    if [ "$(curl -s http://localhost:8080/health)" = '{"status":"UP"}' ]
     then
         exit 0
     else
